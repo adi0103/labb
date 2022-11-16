118bb20bc6afe97115fe85930f36f264be78bde6
ulkshifer <kamza.0508@gmail.com>
2022-11-16 15:41:38 +0600

    third commit

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
-#non functional
-def add_matrices(x, y):
- # результирующая матрица
- result = [
-   [0, 0, 0],
-   [0, 0, 0],
-   [0, 0, 0]
- ]
+from array import array
+def array_sum(nums_arr):
+    sum_n = 0
+    for n in nums_arr:
+        sum_n += n
+    return sum_n
 
- # `len()` возвращает длину списка
- # выполняем итерацию по количеству строк
- for i in range(len(x)):
-   # выполняем итерацию по количеству столбцов
-   for j in range(len(x[0])):
-     result[i][j] = x[i][j] + y[i][j]
-
- return result
-
-# матрица раз
-x = [
- [1, 2, 3],
- [4, 5, 6],
- [7, 8, 9]
-]
-
-# матрица два
-y = [
- [9, 8, 7],
- [3, 2, 1],
- [6, 5, 4]
-]
-
-print(add_matrices(x, y))
-'''
-[
- [10, 10, 10],
- [7, 7, 7],
- [13, 13, 13]
-]
-'''
-
-# данная техника называется представлением списков (list comprehension)
-add_matrices_advanced = lambda x, y: [[x[i][j] + y[i][j] for j in range(len(x[0]))] for i in range(len(x))]
-
-print(add_matrices_advanced(x, y))
+nums = array('i', [1, 2, 3, 4, 5, -15])
+print("Original array:",nums)
+nums_arr = list(map(int, nums))
+result = array_sum(nums_arr)
+print("Sum of all elements of the said array:")
