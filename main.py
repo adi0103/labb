66eca635051e9f15058295a9044b6cf13d8091c9
ulkshifer <kamza.0508@gmail.com>
2022-11-16 15:13:31 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#функция ишинде функция
+def outer_fun(a, b):
+
+    def addition(a, b):
+        return a + b
+
+    add = addition(a, b)
+    return add + 5
+
+result = outer_fun(5, 10)
+print(result)
