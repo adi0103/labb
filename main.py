57a91ca5bdf3f3775e9d9884044f22e789d37b1c
ulkshifer <kamza.0508@gmail.com>
2022-11-23 08:01:28 +0600

    5 commit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def ten_numbers():
-    numbers = []
-    for i in range(1, 11):
-        numbers.append(i)
-    return numbers
-
-
-print(ten_numbers())
+names = ['adilet','ernazar','madi']
+upper_names = list(map(str.upper,names))
+print(upper_names)
