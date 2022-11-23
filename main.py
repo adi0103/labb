ef85c6f31433e74bda4e949f5135e77456b16711
ulkshifer <kamza.0508@gmail.com>
2022-11-23 08:12:56 +0600

    6 commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-names = ['adilet','ernazar','madi']
-upper_names = list(map(str.upper,names))
-print(upper_names)
+def check(letter):
+  list_of_vowels = ['a', 'e', 'i', 'o', 'u']
+  if letter in list_of_vowels:
+      return True
+  else:
+      return False
+letters = ['u', 'a', 'q', 'c', 'i', 'd', 'z', 'p', 'e']
+filtered_object = filter(check, letters)
+filtered_list = list(filtered_object)
+print("The list of vowels is: ", filtered_list)
